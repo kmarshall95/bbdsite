712ab94ef92ba5ea9538136a94cbf5f588a73724
kyle marshall <kyle05marshall2014@gmail.com>
2022-06-23 21:37:10 -0400

    header component build with title

---
+++ src/components/header/Title.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../styles/Title.css';
+
+
+function Title() {
+  return (
+    <div className="Title">
+        <h1>Blue Brothers Designs</h1>
+            <div className="Title-Subtitle">Think Fine Design</div>
+    </div>
+  )
+}
+
+export default Title
